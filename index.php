2b1c0f977fe9825572d010f484a374ef7d7a1eee
Samir <samir.alicherif@etud.u-picardie.fr>
2017-04-03 17:36:04 +0200

    arrangement des liens (style)

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
 						<li data-menuanchor="1stPage" class="active"><a href="#1stPage">LA HALLE</a></li> |
 						<li data-menuanchor="2ndPage"><a href="#2ndPage">LES COMMERÇANTS</a></li> |
 						<li data-menuanchor="3rdPage"><a href="#3rdPage">ACTUALITÉS</a></li> |
-						<li data-menuanchor="4thPage"><a href="#4thPage">INFO/CONTACT</a></li> |
+						<li data-menuanchor="4thPage"><a href="#4thPage">INFO/CONTACT</a></li> 
 					</ul>
 				</nav>	
 			</div>
@@ -61,6 +61,14 @@
 		</div>
 	</main>
 	<script type="text/javascript">
+
+/*$('li a').click(function(){
+	$('li').each(function(i) {
+		$(this).removeClass('active');
+	});
+	$(this).parent().addClass('active');
+});*/
+
 
 $(document).ready(function() {
 	$('#fullpage').fullpage({
