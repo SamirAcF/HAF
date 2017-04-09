17af9eac69dfc7383b866fffbf60446f02c2e557
SamirAcF <samir.alicherif@etud.u-picardie.fr>
2017-04-09 15:20:22 +0200

    on continue à dynamiser l'affichage, réglages graphiques

--- main.php
+++ main.php
@@ -40,10 +40,31 @@ function afficheCom($obj,$tab){?>
 			<h2 class="nomComm"><?php echo $obj->nom; ?></h2>
 		</header>
 		<section class='dispCom'>
-			<p class="descCom"><?php echo $obj->description; ?></p>
-			<?php if($obj->email):?>
-				<p class="descCom">Nous contacter <?php echo $obj->email; ?></p>
+
+
+			<?php if($obj->description): //Vérification de la présence d'une description dans la base?>
+				<p class="descCom"><?php echo $obj->description; ?></p>
+			<?php endif;
+
+			if($obj->email): //Vérification de la présence d'une adresse mail dans la base?>
+				<p class="descCom">Nous contacter :<?php echo $obj->email; ?></p>
+			<?php endif;
+			
+			$fixe=separerFixePortable($obj->telephone)[0]; // on traite les numeros de téléphone
+			$portable=separerFixePortable($obj->telephone)[1];
+			echo $portable.$fixe;
+			if($fixe != "" ): ?>
+				<p class="descCom">Numéro de téléphone :<?php echo $fixe; ?></p>
+			<?php  endif;
+			if($portable != ""):?>
+				<p class="descCom">Numero de portable :<?php echo $portable; ?></p>
+			<?php endif;
+
+			if($obj->horaire == "classique"): //Vérification du fait que les horraire concorde avec ceux de la halle?>
+				<p class="descCom">Nous somme ouverts du Mardi au Jeudi de 9h à 13h et de 15h à 19h, le vendredi et le samedi de 9h à 19h et le dimanche de 9h à 12h30.</p>
 			<?php endif;?>
+			
+
 		</section>
 	</article><?php
 }
@@ -62,5 +83,32 @@ function slide($nom,$var){?>
 		</section>
 	</div><?php
 }
+function separerFixePortable($chaine){
+	$chaine=str_split($chaine);
+	$fixe ="";
+	$portable ="";
+	$toggle=false;
 
+	for ($i=0; $i < count($chaine) ; $i++) {
+		//error_log($fixe." ".$portable."\n", 3, "logs.txt");
+		if($toggle!=true){
+			$fixe .= $chaine[$i];
+			if($chaine[$i] == ","){
+				$fixe=supprimerDernierCarac($fixe);
+				$toggle=true;
+			}
+		}
+		else{
+			$portable .= $chaine[$i];
+		}
+	}	
+	return array($fixe,$portable);
+}
+
+function supprimerDernierCarac($chaine){
+	$chaine=str_split($chaine);
+	array_pop($chaine);
+	implode($retour,$chaine);
+	return $retour;
+}
 ?>
