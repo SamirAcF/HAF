9c28078332c766f8bd90c8dcd4b5eea63ff0eef9
SamirAcF <samir.alicherif@etud.u-picardie.fr>
2017-04-09 20:21:22 +0200

    séparation du menu dans un menu à part pour faciliter la multiplication des pages

---
+++ header.php
@@ -0,0 +1,15 @@
+<!-- HEADER--> 
+	<header id='header'>
+		<div class="grid grid-pad">
+			<div class="col-1-1">
+				<nav id=myMenu>
+					<ul>
+						<li data-menuanchor="1stPage" class="active"><a href="#1stPage">LA HALLE</a></li> |
+						<li data-menuanchor="2ndPage"><a href="#2ndPage">LES COMMERÇANTS</a></li> |
+						<li data-menuanchor="3rdPage"><a href="#3rdPage">ACTUALITÉS</a></li> |
+						<li data-menuanchor="4thPage"><a href="#4thPage">INFO/CONTACT</a></li> 
+					</ul>
+				</nav>	
+			</div>
+		</div>
+	</header>
--- index.php
+++ index.php
@@ -21,22 +21,7 @@
 
 </head>
 <body>
-	<!-- HEADER--> 
-	<header id='header'>
-		<div class="grid grid-pad">
-			<div class="col-1-1">
-				<nav id=myMenu>
-					<ul>
-						<li data-menuanchor="1stPage" class="active"><a href="#1stPage">LA HALLE</a></li> |
-						<li data-menuanchor="2ndPage"><a href="#2ndPage">LES COMMERÇANTS</a></li> |
-						<li data-menuanchor="3rdPage"><a href="#3rdPage">ACTUALITÉS</a></li> |
-						<li data-menuanchor="4thPage"><a href="#4thPage">INFO/CONTACT</a></li> 
-					</ul>
-				</nav>	
-			</div>
-		</div>
-	</header>
-
+<?php include 'header.php';?>
 	<main>
 		<div id="fullpage">
 <!-- 		Première Slide : Accueil du site -->
